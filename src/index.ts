05d1f3b9e1394dd00d47d9c04b476d7face8026f
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-02-05 11:50:48 +0530

    adding more things

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import {server as WebSocketServer} from 'websocket'
+import {connection, Message, server as WebSocketServer} from 'websocket'
 import  http from 'http';
+import { UserManager } from './UserManager';
+import { InitMessageType, SupportedMessage, UpvoteMessageType, UserMessageType , IncomingMessage } from './message';
+import { InMemoryStore } from './store/InMmoryStore';
 
-var server = http.createServer(function(request : any, response : any) {
+const server = http.createServer(function(request : any, response : any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+
+const userManager = new UserManager();
+const store = new InMemoryStore();
+
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
 const wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
 function originIsAllowed(origin:string) {
-  // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
-const = wsServer.on('request', function(request) {
+wsServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
       request.reject();
@@ -36,16 +37,29 @@ const = wsServer.on('request', function(request) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message:any) {
+        // add rate limit
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            try {
+                messageHandler(connection,JSON.parse(message.utf8Data));
+            } catch(e){
+
+            }
+            // console.log('Received Message: ' + message.utf8Data);
+            // connection.sendUTF(message.utf8Data);   
         }
     });
     connection.on('close', function(reasonCode : any, description : any) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(ws: connection , message : IncomingMessage){
+    if (message.type == SupportedMessage.JoinRoom){
+        const payload = message.payload;
+        userManager.addUser(payload.name , payload.roomId , payload.roomId , ws);
+    }
+
+    if (message.type === SupportedMessage.SendMessage){
+        const payload = message.payload;
+    }
+}
